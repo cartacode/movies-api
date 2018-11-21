12665729b3dba0f1c43556640daa80bb60484515
it-avenger <simbawow8@gmail.com>
2018-11-21 10:06:59 -0800

    Added fields to customer

--- models/customer.go
+++ models/customer.go
@@ -20,15 +20,58 @@ type Customer struct {
 	// read only: true
 	Email string `json:"email"`
 
-	// Credit Information
-	Credit *CustomerCredit `json:"credit"`
-
-	// Purchased Items
-	Purchased *CustomerPurchased `json:"purchased"`
-
 	// True/False. Is the user active
 	Active bool `json:"active"`
 
+	// Liked Items
+	Liked struct {
+
+		// List of Mongo ObjectID for the movies wish list. Embeddable
+		Movies []string `json:"movies"`
+
+		// List of Mongo ObjectID for the scenes wish list. Embeddable
+		Scenes []string `json:"scenes"`
+
+		// List of Mongo ObjectID for the volumes wish list. Embeddable
+		Volumes []string `json:"volumes"`
+
+		// List of Mongo ObjectID for the stars wish list. Embeddable
+		Stars []string `json:"stars"`
+	} `json:"liked"`
+
+	// Credit Information
+	Credit struct {
+
+		// Has the user stored credit information
+		InfoStored bool `json:"info_stored"`
+
+		// Key for 3 leg transactions to provider bank
+		Key string `json:"key"`
+	} `json:"credit"`
+
+	// Purchased Items
+	Purchased struct {
+
+		// List of Mongo ObjectID for the movies wish list. Embeddable
+		Movies []string `json:"movies"`
+
+		// List of Mongo ObjectID for the scenes wish list. Embeddable
+		Scenes []string `json:"scenes"`
+
+		// List of Mongo ObjectID for the volumes wish list. Embeddable
+		Volumes []string `json:"volumes"`
+	} `json:"purchased"`
+
 	// User wishlist
-	Wishlist *CustomerWishlist `json:"wishlist"`
+	Wishlist struct {
+
+		// List of Mongo ObjectID for the movies wish list. Embeddable
+		Movies []string `json:"movies"`
+
+		// List of Mongo ObjectID for the scenes wish list. Embeddable
+		Scenes []string `json:"scenes"`
+
+		// List of Mongo ObjectID for the volumes wish list. Embeddable
+		Volumes []string `json:"volumes"`
+	} `json:"wishlist"`
 }
